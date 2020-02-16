4aa00abe45cc6a69131d4d18cdda1be570ce0de8
club24 <61007674+club24@users.noreply.github.com>
2020-02-16 17:08:41 +0800

    created level and added platorms

---
+++ yest 1/Assets/Scripts/VanishingPlat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishingPlat : MonoBehaviour
+{
+    public float Magic = 4;
+    public float Seconds = 2;
+
+    Renderer myR;
+    Collider myC;
+
+    void OnTriggerEnter(Collider other)
+    {
+        myR = GetComponent<Renderer>();
+        myC = GetComponent<Collider>();
+
+        if (other.gameObject.name == "Player")
+        {
+            StartCoroutine(Poof());
+            StartCoroutine(Wizard());
+        }
+    }
+
+    IEnumerator Poof()
+    {
+        yield return new WaitForSeconds(Seconds);
+        myR.enabled = false;
+        myC.enabled = false;
+        //Destroy(gameObject);
+    }
+
+    IEnumerator Wizard()
+    {
+        yield return new WaitForSeconds(Magic);
+        myR.enabled = true;
+        myC.enabled = true;
+        //gameObject.SetActive(true);
+    }
+}
